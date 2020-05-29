a717db98b20cc801eb71c7bc499f3b596d701d50
Choi Jiwon <winning1993@naver.com>
2020-05-30 03:03:56 +0900

    기본기능 구현

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 const settingBtn = document.getElementById("settingBtn"),
-settingArea = document.getElementById("settingArea")
+settingArea = document.getElementById("settingArea"),
+keyword = document.getElementById("keyword"),
+answerInput = document.getElementById("answerInput"),
+leftTime = document.getElementById("leftTime"),
+score = document.getElementById("score"),
+words = ["참다","크기","고기","남기다","서양","주요","지나치다","가져오다","냄새",
+"부드럽다","여기다","공연","남녀","내놓다","만들어지다","심각하다","준비","계속되다",
+"대답하다","대담하다","업무","입시","준비하다","청소년"]
+
+
+let time = 0,
+scoreValue = 0;
+
+function showKeyword(){
+    const randomNum = Math.floor(Math.random()*words.length)
+    keyword.innerHTML = words[randomNum]
+}
 
 function handleSettingBtn(){
     settingArea.style.animation = "showSetting 0.3s forwards";
@@ -15,8 +31,21 @@ function handlehideSetting(){
     settingBtn.removeEventListener("click", handlehideSetting);
 }
 
+function handleTyping(e){
+    
+    if(e.target.value === keyword.innerHTML){
+        e.target.value = null;
+        showKeyword()
+        leftTime.innerHTML = `남은시간: ${time+=1}`;
+        score.innerHTML = `스코어: ${scoreValue+=1}`;
+        console.log(time)
+    }
+}
+
 function init(){
+    showKeyword()
     settingBtn.addEventListener("click", handleSettingBtn);
+    answerInput.addEventListener("textInput", handleTyping)
 }
 
 init();
