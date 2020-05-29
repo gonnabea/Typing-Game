0ae9d026a90563f1b1cbc2e2dd3a5201bdb2efac
Choi Jiwon <winning1993@naver.com>
2020-05-30 03:09:29 +0900

    난이도 설정

--- script.js
+++ script.js
@@ -4,13 +4,15 @@ keyword = document.getElementById("keyword"),
 answerInput = document.getElementById("answerInput"),
 leftTime = document.getElementById("leftTime"),
 score = document.getElementById("score"),
+difficulty = document.getElementById("difficulty")
 words = ["참다","크기","고기","남기다","서양","주요","지나치다","가져오다","냄새",
 "부드럽다","여기다","공연","남녀","내놓다","만들어지다","심각하다","준비","계속되다",
 "대답하다","대담하다","업무","입시","준비하다","청소년"]
 
 
 let time = 0,
-scoreValue = 0;
+scoreValue = 0,
+difficultyIndex = 1;
 
 function showKeyword(){
     const randomNum = Math.floor(Math.random()*words.length)
@@ -36,16 +38,29 @@ function handleTyping(e){
     if(e.target.value === keyword.innerHTML){
         e.target.value = null;
         showKeyword()
-        leftTime.innerHTML = `남은시간: ${time+=1}`;
-        score.innerHTML = `스코어: ${scoreValue+=1}`;
+        leftTime.innerHTML = `남은시간: ${time+=difficultyIndex}`;
+        score.innerHTML = `스코어: ${scoreValue+=difficultyIndex}`;
         console.log(time)
+    }
+}
+
+function handleDifficulty(){
+    if(difficulty.value === "easy"){
+        difficultyIndex = 1
+    }
+    else if(difficulty.value === "medium"){
+        difficultyIndex = 2
+    }
+    else{
+        difficultyIndex = 3
     }
 }
 
 function init(){
     showKeyword()
     settingBtn.addEventListener("click", handleSettingBtn);
-    answerInput.addEventListener("textInput", handleTyping)
+    answerInput.addEventListener("input", handleTyping);
+    difficulty.addEventListener("change", handleDifficulty)
 }
 
 init();
